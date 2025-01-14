d478260036c2f04083b12c0222a3324ed8a42565
chamiims <caitouaggo@gmail.com>
2025-01-14 15:20:12 +0100

    exam js

---
+++ extension.js
@@ -0,0 +1,24 @@
+/*Exercice 1*/
+
+let prenom = "Chaïma";
+let nom = "Aït Ouaggo";
+let age = "21";
+let phrase = "Je m'appelle " + prenom + " " + nom  + " et j'ai " + age + " ans ";
+console.log (phrase)
+alert(phrase)
+
+/*Exercice2*/
+
+let a = 6;
+let b = 4;
+let somme = a + b;
+console.log (somme)
+
+let différence = a - b;
+console.log (différence)
+
+let produit = a * b;
+console.log (produit)
+
+let quotient = a / b;
+console.log (quotient)
